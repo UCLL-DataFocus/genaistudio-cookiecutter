d3f45f72a804200e2a14900aa398a2491a3ac338
Bexj <joren.bex@student.ucll.be>
2025-04-01 10:49:54 +0000

    deleted chat

--- {{ cookiecutter.__project_name_kebab_case }}/app.py
+++ {{ cookiecutter.__project_name_kebab_case }}/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 {%- if cookiecutter.include_llm|int %}
 from src.components.model_selection import show_llm_ui
-from src.components.chat import show_chat_interface
 {% endif %}
 
 {% if cookiecutter.include_llm|int %}
@@ -31,9 +30,7 @@ def app():
     )
 
     {%- if cookiecutter.include_llm|int %}
-    # Show LLM selection UI
     show_llm_ui()
-    show_chat_interface()
     {%- endif %}
   
 if __name__ == "__main__":
